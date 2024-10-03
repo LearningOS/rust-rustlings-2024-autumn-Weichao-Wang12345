4e3e8d19abc2ef2ee37755e2ecbc3f40d5733eb4
Weichao-Wang12345 <2639840196@qq.com>
2024-10-03 23:51:42 +0800

    update

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -3,7 +3,9 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::f64::consts::E;
+
+
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -17,6 +19,13 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
+        if value < 0
+        {
+           return  Err(CreationError::Negative);
+        }else if value == 0
+        {
+           return  Err(CreationError::Zero);
+        }
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
