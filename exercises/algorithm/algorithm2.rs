98f5714b12a1d337bdbe1d307e8cae60a95a6cda
Weichao-Wang12345 <2639840196@qq.com>
2024-10-15 23:12:53 +0800

    update

--- exercises/algorithm/algorithm2.rs
+++ exercises/algorithm/algorithm2.rs
@@ -73,6 +73,18 @@ impl<T> LinkedList<T> {
     }
 	pub fn reverse(&mut self){
 		// TODO
+        let mut current = self.start;
+        let mut prev = None;
+        while let Some(node) = current {
+            let next = unsafe {
+                (*node.as_ptr()).next
+            };
+            unsafe {(*node.as_ptr()).next=prev};
+            unsafe {(*node.as_ptr()).prev=next};
+            prev=Some(node);
+            current=next;
+        }
+        self.start = prev;
 	}
 }
 
